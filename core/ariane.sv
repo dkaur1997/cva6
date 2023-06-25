cad4e95e59501537fdb1267e68b98da38520bd7e
Divjot <d2kaur@uwaterloo.ca>
2023-06-25 17:11:53 -0400

    debug_analysis_1

--- core/ariane.sv
+++ core/ariane.sv
@@ -39,8 +39,8 @@ module ariane import ariane_pkg::*; #(
   input  logic                         time_irq_i,   // timer interrupt in (async)
   input  logic                         debug_req_i,  // debug request (async)
   //EVU
-  input ariane_axi::req_t axi_evu_conf_req_i, //AXI input to evu_top
-  output ariane_axi::resp_t axi_evu_conf_resp_o, //AXI output to evu_top
+  input ariane_axi::req_t axi_evu_cfg_req_i, //AXI input to evu_top
+  output ariane_axi::resp_t axi_evu_cfg_resp_o, //AXI output to evu_top
   SPU_INTF.Output        evu_output,
 `ifdef FIRESIM_TRACE
   // firesim trace port
@@ -1035,8 +1035,9 @@ module ariane import ariane_pkg::*; #(
 // ------------
   // EVU_top
   // ------------
-  evu_top #(.ASID_WIDTH ( ASID_WIDTH ))
-  evu_top_i (.clk_i(clk_i), .rst_ni(rst_ni), .commit_instr_i(commit_instr_id_commit), .commit_ack_i(commit_ack), 
+  evu_top #(.ASID_WIDTH ( ASID_WIDTH ),
+  )
+  evu_top_i (.axi_evu_cfg_req_i(axi_evu_cfg_req_i), axi_evu_cfg_resp_o(axi_evu_cfg_resp_o),.clk_i(clk_i), .rst_ni(rst_ni), .commit_instr_i(commit_instr_id_commit), .commit_ack_i(commit_ack), 
   .l1_icache_miss_i(icache_miss_cache_perf), .l1_dcache_miss_i(dcache_miss_cache_perf), .itlb_miss_i(itlb_miss_ex_perf), 
   .dtlb_miss_i(dtlb_miss_ex_perf), .sb_full_i(sb_full), .if_empty_i(~fetch_valid_if_id), 
   .ex_i(ex_commit), .eret_i(eret), .resolved_branch_i(resolved_branch), .evu_ouput(evu_ouput), .priv_lvl_i(priv_lvl), .asid_i(asid_csr_ex));
